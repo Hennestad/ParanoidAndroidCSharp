d02a313686932cdd441b5fe9479401dac3aabf71
Hennestad <59829447+Hennestad@users.noreply.github.com>
2022-11-10 00:25:53 +0100

    newsnewsnewsnews

--- ParanoidAndroid/Modules/News.cs
+++ ParanoidAndroid/Modules/News.cs
@@ -4,8 +4,9 @@ using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
 using System.IO;
-using System.Xml;
 using System.Reflection.Emit;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace ParanoidAndroid.Modules
 {
@@ -15,35 +16,37 @@ namespace ParanoidAndroid.Modules
         [Alias("nyheter")] // Aliases that will also trigger the command.
         [Summary("Make the bot post the latest news.")] // Command summary.
 
-        public async Task NrkNews()
+        public async Task NrkNews([Remainder] string categoryInput)
         {
             //Create the XmlDocument.
-            XmlDocument doc = new XmlDocument();
-            doc.Load("https://www.nrk.no/nyheter/siste.rss");
+            XDocument nodeList = XDocument.Load("https://www.nrk.no/nyheter/siste.rss");
 
-            //Display all the titles.
-            XmlNodeList nodeList = doc.GetElementsByTagName("item");
-            foreach (XmlNode xmlNode in nodeList)
+            //Get all the descendant elements of the item elements.
+            foreach (XElement element in nodeList.Descendants("item").Where(x => x.Element("category")?.Value == categoryInput))
             {
-                string titleText = xmlNode["title"].InnerText;
-                string descriptionText = xmlNode["description"].InnerText;
-                string urlText = xmlNode["link"].InnerText;
-                string pubDateText = xmlNode["pubDate"].InnerText;
-                //string categoryText = xmlNode["category"].InnerText;
-                //string thumbnailUrlText = "https://upload.wikimedia.org/wikipedia/commons/8/87/NRK_Nyheter.png";
-                string imageUrlText = "No Image"; 
+                if (element != null)
+                {
+                    XNamespace media = "http://search.yahoo.com/mrss/";
+
+                string titleText = element.Element("title").Value;
+                string descriptionText = element.Element("description").Value;
+                string urlText = element.Element("link").Value;
+                //string pubDateText = element.Element("pubdate").Value;
+                //string categoryText = element.Element("category").Value;
+               // string thumbnailUrlText = "https://upload.wikimedia.org/wikipedia/commons/8/87/NRK_Nyheter.png";
+                string imageUrlText = "No Image";
 
                 //If image attribute is null, replace it with a default image. 
-                if (xmlNode["media:content"]!=null)
+                if (element.Element(media + "content") != null)
                 {
-                    imageUrlText = xmlNode["media:content"].Attributes["url"].Value;
+                    imageUrlText = element.Element(media + "content").Attribute("url").Value;
                 }
                 else
                 {
                     imageUrlText = "https://static.nrk.no/nrkno/serum/2.0.476/common/img/nrk-logo-white-72x26.png";
                 }
-                    
-                var embed = new EmbedBuilder
+
+                var news = new EmbedBuilder
                 {
                     // Embed property can be set within object initializer
                     Color = Color.Blue,
@@ -54,13 +57,28 @@ namespace ParanoidAndroid.Modules
                     ImageUrl = imageUrlText,
                 };
                 // Or with methods
-                embed.WithAuthor("NRK");
-                embed.WithFooter(footer => footer.Text = pubDateText);
+                news.WithAuthor("Siste nytt – NRK");
+                //embed.WithFooter(footer => footer.Text = pubDateText);
                 //    embed.WithCurrentTimestamp();
 
 
                 //Your embed needs to be built before it is able to be sent
-                await ReplyAsync(embed: embed.Build());
+                await ReplyAsync(embed: news.Build());
+
+                }
+
+                else
+                {
+                    var noNews = new EmbedBuilder
+                    {
+                        Color = Color.Red,
+                        Title = "No News",
+                        Description = "No news on this topic right now.",
+                    };
+                    noNews.WithAuthor("NRK");
+
+                    await ReplyAsync(embed: noNews.Build());
+                }
             }
 
         }
