0f81e9628a04aace68424377a3df2e928db5dd36
Hennestad <59829447+Hennestad@users.noreply.github.com>
2022-11-25 16:03:12 +0100

    Update News.cs

--- ParanoidAndroid/Modules/News.cs
+++ ParanoidAndroid/Modules/News.cs
@@ -28,36 +28,37 @@ namespace ParanoidAndroid.Modules
                 {
                     XNamespace media = "http://search.yahoo.com/mrss/";
 
-                string titleText = element.Element("title").Value;
-                string descriptionText = element.Element("description").Value;
-                string urlText = element.Element("link").Value;
-                string imageUrlText = "No Image";
+                    string titleText = element.Element("title").Value;
+                    string descriptionText = element.Element("description").Value;
+                    string urlText = element.Element("link").Value;
+                    string imageUrlText = "No Image";
 
-                //If image attribute is null, replace it with a default image. 
-                if (element.Element(media + "content") != null)
-                {
-                    imageUrlText = element.Element(media + "content").Attribute("url").Value;
-                }
-                else
-                {
-                    imageUrlText = "https://static.nrk.no/nrkno/serum/2.0.476/common/img/nrk-logo-white-72x26.png";
-                }
+                    //If image attribute is null, replace it with a default image. 
+                    if (element.Element(media + "content") != null)
+                    {
+                        imageUrlText = element.Element(media + "content").Attribute("url").Value;
+                    }
+                    else
+                    {
+                        imageUrlText = "https://static.nrk.no/nrkno/serum/2.0.476/common/img/nrk-logo-white-72x26.png";
+                    }
 
-                var news = new EmbedBuilder
-                {
-                    // Embed property can be set within object initializer
-                    Color = Color.Blue,
-                    Title = titleText,
-                    Description = descriptionText,
-                    Url = urlText,
-                    ImageUrl = imageUrlText,
-                };
-                // Or with methods
-                news.WithAuthor("Siste nytt – NRK");
+                    var news = new EmbedBuilder
+                    {
+                        // Embed property can be set within object initializer
+                        Color = Color.Blue,
+                        Title = titleText,
+                        Description = descriptionText,
+                        Url = urlText,
+                        ImageUrl = imageUrlText,
+                    };
+
+                    // Or with methods
+                    news.WithAuthor("Siste nytt – NRK");
 
 
-                //Your embed needs to be built before it is able to be sent
-                await ReplyAsync(embed: news.Build());
+                    //Your embed needs to be built before it is able to be sent
+                    await ReplyAsync(embed: news.Build());
 
                 }
             }
